7516b2f81af91e12b8606c0dc53c7c3413375ad3
Anshul Bari <148080470+AnshulBari@users.noreply.github.com>
2026-07-06 10:47:32 +0530

    Time: 0 ms (100%), Space: 13.7 MB (91.69%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+
+        int n = chars.size();
+
+        int index = 0;      // write pointer
+        int i = 0;          // read pointer
+
+        while (i < n) {
+
+            char current = chars[i];
+            int count = 0;
+
+            // Count consecutive characters
+            while (i < n && chars[i] == current) {
+                count++;
+                i++;
+            }
+
+            // Write character
+            chars[index++] = current;
+
+            // Write count if greater than 1
+            if (count > 1) {
+
+                string s = to_string(count);
+
+                for (char c : s)
+                    chars[index++] = c;
+            }
+        }
+
+        return index;
+    }
+};
